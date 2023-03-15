1fa78a5b5ac33c2e0f838a9b415fbb1dee490691
Rafael José <ra.david@hotmail.com>
2023-03-15 19:30:02 +0000

    Exercise 1
    Add question that asks the user to enter a character

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -8,7 +8,13 @@ namespace ChangeString
         {
             // Ask the user to enter a string
             Console.Write("Please enter a string: ");
-            string FirstQuestion = Console.ReadLine();
+            string firstQuestion = Console.ReadLine();
+
+            // Ask the user to enter a character
+            Console.Write("Please enter a character: ");
+            string secondQuestion = Console.ReadLine();
+            // Convert the input to a character
+            char character = char.Parse(secondQuestion);
         }
     }
 }
