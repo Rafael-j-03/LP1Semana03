29cd9905adc153d80add614cbc37b29d005bcd34
Rafael José <ra.david@hotmail.com>
2023-03-15 19:20:00 +0000

    Exercise 1
    Add project ChangeString
    
    Add question that asks the user to enter a string

---
+++ ChangeString/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user to enter a string
+            Console.Write("Please enter a string: ");
+            string FirstQuestion = Console.ReadLine();
+        }
+    }
+}
