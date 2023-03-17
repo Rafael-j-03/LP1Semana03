e4e7baaa593535d11a57f48e43456afcc2ad49b5
Rafael José <ra.david@hotmail.com>
2023-03-17 13:33:12 +0000

    Exercise 5
    Ask the user the number of achievements for each player

--- PlayerAchievements/Program.cs
+++ PlayerAchievements/Program.cs
@@ -6,16 +6,23 @@ namespace PlayerAchievements
     {
         static void Main(string[] args)
         {
-            Achievements playerAchievements = 0;
+            //Achievements playerAchievements = 0;
             
             // Ask the user the number of players and turn it into a int
             Console.Write("Enter the number of players: ");
             string numberOfPlayersQ = Console.ReadLine();
             int n = Convert.ToInt32(numberOfPlayersQ);
-
                         
             // Create array for achievements
             int [] achievements = new int [n];
+
+            // Ask the user the number of achievements for each player
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Enter the number of achievements for player {i + 1}: ");
+                string givenAchievements = Console.ReadLine();
+                int nGivenAchievements = Convert.ToInt32(givenAchievements);
+            }
         }
     }
 }
